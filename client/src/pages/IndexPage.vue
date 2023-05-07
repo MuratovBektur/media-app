1d182017896429fd8ca4f7d6489a94990a850a61
Bektur <prostreyd@gmail.com>
2023-05-07 23:12:22 +0600

    feat: add node_env for server

--- client/src/pages/IndexPage.vue
+++ client/src/pages/IndexPage.vue
@@ -5,8 +5,8 @@
 
 <script setup lang="ts">
 import { api } from 'src/boot/axios';
-import axios from 'axios';
 import { ref } from 'vue';
+import axios from 'axios';
 
 const todos = ref<any>([]);
 const localData = ref<any>();
@@ -16,7 +16,7 @@ async function getTodoList() {
 }
 async function check() {
   console.log('in');
-  const res = await axios('http://192.168.0.106:4500');
+  const res = await axios('http://192.168.0.106:8443');
   localData.value = res.data.msg;
   // alert(res.data.msg);
 }
